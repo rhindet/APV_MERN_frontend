d2fd8d01b1226fef1109e8ae643089b62e739001
rhindet <luisrcparra2@gmail.com>
2022-09-28 12:48:07 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
+const Header = () => {
+    const {cerrarSesion} = useAuth()
+  return (
+    
+    <header className="py-10 bg-indigo-600">
+        <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <h1 className="font-bold text-2xl text-indigo-200 text-center"> APV - Administrador de Pacientes de 
+            {' '}
+                <span className="text-white font-black">Veterinaria</span>
+            </h1>
+            <nav className="flex flex-col items-center lg:flex-row gap-4 mt-5 lg:mt-0">
+                <Link to ="/admin" className="text-white text-sm uppercase font-bold">PACIENTES</Link>
+                <Link to ="/admin/perfil" className="text-white text-sm uppercase font-bold">PERFIL</Link>
+                    <button
+                       className="text-white text-sm uppercase font-bold"
+                       onClick={cerrarSesion}
+                    >CERRAR SESIÓN</button>
+            </nav>
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
---
+++ src/layout/RutaProtegida.jsx
@@ -0,0 +1,26 @@
+import { Outlet ,Navigate} from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+const RutaProtegida = () => {
+    const {auth,cargando}= useAuth()
+
+   
+    if(cargando) return ''
+  return (
+    <>
+      <Header/>
+         
+          {auth?._id ? (
+              <main className="container mx-auto mt-10">
+                <Outlet/>
+              </main>
+          ) : <Navigate to="/"/>}
+      <Footer/> 
+
+    </>
+  )
+}
+
+export default RutaProtegida
